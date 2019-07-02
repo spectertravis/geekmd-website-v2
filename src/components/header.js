622ecb75dd7a12fe1622037eb3fc7d6990d6f23b
Travis Nesbit <travis.nesbit@spectertechnology.com>
2019-07-01 21:46:06 -0700

    Removed tech from home header

--- src/components/header.js
+++ src/components/header.js
@@ -10,23 +10,6 @@ const Header = () => (
           <div className="header__cta-text">
               <h1 className="heading-primary font-code">
                   The Future of Health Information Technology is Open Source. We are better, together.
-                  <span className="header__subtext">
-                      <a
-                          href="https://en.wikipedia.org/wiki/HTTP/2"
-                          title="Link to HTTP/2 Wiki Entry">
-                          HTTP/2
-                      </a>{' | '}
-                      <a
-                          href="https://developers.google.com/protocol-buffers/"
-                          title="Link to Protocol Buffers Page">
-                          Protocol Buffers
-                      </a>{' | '}
-                      <a
-                          href="https://grpc.io/"
-                          title="Link to gRPC website">
-                          gRPC
-                      </a>
-                  </span>
               </h1>
           </div>
       </div>
